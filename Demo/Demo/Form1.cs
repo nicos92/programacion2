02d607c3352d78742f3a8a60cb62181d5a8f386b
nicos92 <nicolassandoval140692@gmail.com>
2024-11-02 21:42:49 -0300

    guia n1

---
+++ Demo/Demo/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Si";
+            label2.Text = "Si";
+
+            label3.Text = "Si";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "No";
+            label2.Text = "No";
+
+            label3.Text = "No";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "A";
+            label2.Text = "B";
+
+            label3.Text = "C";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Adios Mundo!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+
+            label3.Visible = false;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+
+            label3.Visible = true;
+
+        }
+    }
+}
