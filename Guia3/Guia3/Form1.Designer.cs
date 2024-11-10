0ba95d9f740688fb958c920ac344689eeeb59687
nicos92 <nicolassandoval140692@gmail.com>
2024-11-10 20:08:07 -0300

    terminando guia 3

--- Guia3/Guia3/Form1.Designer.cs
+++ Guia3/Guia3/Form1.Designer.cs
@@ -32,30 +32,32 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TbIngreso = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.LblDolares = new System.Windows.Forms.Label();
+            this.LblCentavos = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 33);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(87, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 26);
+            this.label1.Size = new System.Drawing.Size(218, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Introdusca la cantidad de centavos: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(88, 145);
+            this.label2.Location = new System.Drawing.Point(87, 138);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(199, 13);
             this.label2.TabIndex = 1;
@@ -69,15 +71,15 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.36364F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.63636F));
             this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TbIngreso, 2, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 1, 5);
             this.tableLayoutPanel1.Controls.Add(this.label4, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.label2, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.button1, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label5, 2, 4);
-            this.tableLayoutPanel1.Controls.Add(this.label6, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.LblDolares, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.LblCentavos, 2, 5);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 17);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 7;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.703704F));
@@ -87,24 +89,28 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.51852F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.51852F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.703704F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(624, 361);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(618, 341);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
-            // textBox1
+            // TbIngreso
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(99)))), ((int)(((byte)(69)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(239)))), ((int)(((byte)(233)))));
-            this.textBox1.Location = new System.Drawing.Point(314, 35);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 21);
-            this.textBox1.TabIndex = 5;
+            this.TbIngreso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
+            this.TbIngreso.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TbIngreso.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TbIngreso.Font = new System.Drawing.Font("MartianMono NF", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbIngreso.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(239)))), ((int)(((byte)(233)))));
+            this.TbIngreso.Location = new System.Drawing.Point(311, 56);
+            this.TbIngreso.MaxLength = 18;
+            this.TbIngreso.Name = "TbIngreso";
+            this.TbIngreso.Size = new System.Drawing.Size(218, 16);
+            this.TbIngreso.TabIndex = 5;
+            this.TbIngreso.TextChanged += new System.EventHandler(this.TbIngreso_TextChanged);
+            this.TbIngreso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbIngreso_KeyPress);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(88, 277);
+            this.label3.Location = new System.Drawing.Point(87, 264);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 13);
             this.label3.TabIndex = 3;
@@ -113,7 +119,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(88, 211);
+            this.label4.Location = new System.Drawing.Point(87, 201);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 4;
@@ -121,41 +127,60 @@
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(92)))), ((int)(((byte)(18)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Enabled = false;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(56)))), ((int)(((byte)(33)))));
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(56)))), ((int)(((byte)(33)))));
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(111)))), ((int)(((byte)(80)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(314, 82);
+            this.button1.Location = new System.Drawing.Point(378, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 25);
             this.button1.TabIndex = 6;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LblDolares
+            // 
+            this.LblDolares.AutoSize = true;
+            this.LblDolares.Font = new System.Drawing.Font("MartianMono NF", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDolares.Location = new System.Drawing.Point(311, 201);
+            this.LblDolares.Name = "LblDolares";
+            this.LblDolares.Size = new System.Drawing.Size(17, 16);
+            this.LblDolares.TabIndex = 7;
+            this.LblDolares.Text = "$";
+            // 
+            // LblCentavos
+            // 
+            this.LblCentavos.AutoSize = true;
+            this.LblCentavos.Font = new System.Drawing.Font("MartianMono NF", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCentavos.Location = new System.Drawing.Point(311, 264);
+            this.LblCentavos.Name = "LblCentavos";
+            this.LblCentavos.Size = new System.Drawing.Size(17, 16);
+            this.LblCentavos.TabIndex = 8;
+            this.LblCentavos.Text = "¢";
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // label5
+            // groupBox1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(314, 211);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(15, 13);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "$";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(314, 277);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(15, 13);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "¢";
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(239)))), ((int)(((byte)(233)))));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(624, 361);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Form Guia 3";
             // 
             // Form1
             // 
@@ -163,15 +188,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(624, 361);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("MartianMono NF", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Guia 3";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -183,11 +211,12 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TbIngreso;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LblDolares;
+        private System.Windows.Forms.Label LblCentavos;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Guia3/Guia3/Form1.cs
+++ Guia3/Guia3/Form1.cs
@@ -16,5 +16,56 @@ namespace Guia3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long centavos;
+            centavos = long.Parse(TbIngreso.Text);
+            LblDolares.Text = "$ " + Convert.ToString(centavos / 100);
+            LblCentavos.Text = "C "+  Convert.ToString(centavos % 100);
+        }
+
+        private void TbIngreso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == true)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+
+            }
+            if (e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+
+
+        }
+
+        private void EnabledButtonCalcular()
+        {
+            if (TbIngreso.Text.Length > 0)
+            {
+                EnabledCalcular(true);
+            }
+            else
+            {
+                EnabledCalcular(false);
+
+            }
+        }
+
+        private void EnabledCalcular(bool enabled)
+        {
+            button1.Enabled = enabled;
+        }
+
+        private void TbIngreso_TextChanged(object sender, EventArgs e)
+        {
+            EnabledButtonCalcular();
+
+        }
     }
 }
